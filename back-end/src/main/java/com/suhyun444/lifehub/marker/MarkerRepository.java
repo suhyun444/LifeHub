251f26b49343ec46f03abe1f30f2d7ba4b58aea8
Suhyun444 <windseed71@gmail.com>
2026-02-11 14:27:24 +0900

    feat : Marker 최신순으로 가져오도록 변경

--- back-end/src/main/java/com/suhyun444/lifehub/marker/MarkerRepository.java
+++ back-end/src/main/java/com/suhyun444/lifehub/marker/MarkerRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.suhyun444.lifehub.marker.Entity.Marker;
 
 public interface MarkerRepository extends JpaRepository<Marker,Long>{
-    List<Marker> findByUserId(Long userId);
+    List<Marker> findByUserIdOrderByIdDesc(Long userId);
 }
